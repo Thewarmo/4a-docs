90dd0b3aeab28b0c39d955a09d4ea105350af7fa
MarceGalan <marcelagalansalazar@gmail.com>
2021-12-06 14:55:17 -0500

    Cambio peque

--- 4a-docs/api gateway/src/server.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    auth_api_url: 'https://mision-tic-sup-be.herokuapp.com/',
-    account_api_url: 'https://ms-sp-products.herokuapp.com/',
-    productos_api_url: 'https://ms-sp-products.herokuapp.com/',
-    };
-    //urls de la aplicacion
