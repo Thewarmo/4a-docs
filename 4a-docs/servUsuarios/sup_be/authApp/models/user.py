e081ede678f4fb4dddb79cc7ded8f1facd62ac6f
mcsanguinod <camidolphin@gmail.com>
2021-12-06 19:32:07 -0500

    agregar rol a usuario

--- 4a-docs/servUsuarios/sup_be/authApp/models/user.py
+++ 4a-docs/servUsuarios/sup_be/authApp/models/user.py
@@ -32,6 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     password = models.CharField('Password', max_length = 256)
     name = models.CharField('Name', max_length = 30)
     email = models.EmailField('Email', max_length = 150)
+    rol = models.CharField('Rol',max_length=20)
 
     def save(self, **kwargs):
         some_salt = 'mMUj0DrIK6vgtdIYepkIxN' 
